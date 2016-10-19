a06b725ea62902fd6d09bb53cb91a30685e19ffe
Chen, Hung <Hung.Chen@Pearson.com>
2016-10-19 14:08:48 -0400

    initial checkin of front-end code

--- web/js/index.js
+++ web/js/index.js
@@ -1,7 +1,21 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import './indexstyles.scss';
+// not sure why i need to do this but there is a warning about it
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
-let socket = io('http://localhost:3000');
-
-socket.on('project:list', (result) => {
-	console.log(result);
-});
+ReactDOM.render(
+	<MuiThemeProvider>
+		<div>
+			<AppBar
+				className="appBar"
+				title="Plucky" 
+				style={{ backgroundColor: 'green' }} />
+			<div className="container-fluid app-body" style={{ paddingTop:'20px' }}>
+			</div>
+		</div>
+	</MuiThemeProvider>
+, document.getElementById('entry'));
