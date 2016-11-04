cd9aea5c58e171afb6fc4929dc7aa2a7e9fb7cc7
Chen, Hung <Hung.Chen@Pearson.com>
2016-11-04 12:36:27 -0400

    changed configuration to be UI based

--- src/controllers/pipelineController.js
+++ src/controllers/pipelineController.js
@@ -4,33 +4,53 @@ const {Pipeline} = require('plucky-pipeliner');
 const {plugins} = require('plucky-plugin-manager');
 const {jsonMapper} = require('plucky-mapper');
 
-
 class ProjectController {
 	constructor(options) {
 		this.socket = options.socket;
-		this.socket.on('pipeline:start', this.executePipeline.bind(this));
+		this.socket.on('build:start', this.executeBuild.bind(this));
+		this.socket.on('pipeline:get', this.getProjectPipeline.bind(this));
 	}
 
-	executePipeline(project) {
+	executeBuild(project) {
 		plugins.get(project.imports, (err, obj) => {
 			// any web overrides will be done here
-			const processOverrides = jsonMapper(project, config);
-			const pipeline = new Pipeline({
+			const processOverrides = jsonMapper(project.build, config);
+			const build = new Pipeline({
 				name: project.name,
 				description: project.description,
 				tasks: obj,
 				process: processOverrides.process
 			});
-			pipeline.execute({}, (error, result) => {
-				this.socket.emit('pipeline:completed', {error, project});
+			build.execute({}, (error, result) => {
+				console.log('build completed');
+				this.socket.emit('build:completed', {error, project});
 			});
-			pipeline.on('progress', (progress) => {
-				this.socket.emit('pipeline:progress', progress);
+			build.on('progress', (progress) => {
+				console.log('build progress');
+				this.socket.emit('build:progress', progress);
 			});
-			pipeline.on('steperror', (error) => {
-				console.log(error);
+			build.on('steperror', (error) => {
+				console.log('build steperror');
+				this.socket.emit('build:steerror', error);
 			});
 		});
+	}
+
+	getProjectPipeline(project) {
+		plugins.get(project.imports, (err, obj) => {
+			const processOverrides = jsonMapper(project.pipeline, config);
+			const pipeline = new Pipeline({
+				tasks: obj,
+				process: processOverrides.process
+			});
+			pipeline.execute({}, (error, val) => {
+				console.log('pipelie list');
+				this.socket.emit('pipeline:list', val.result);
+			});
+			
+
+		});
+		
 	}
 }
 
