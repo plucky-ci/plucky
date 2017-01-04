9850866982de27d677398eb1305ff52b66162325
Chen, Hung <Hung.Chen@Pearson.com>
2017-01-04 16:45:02 -0500

    added bitesize files as the environment files.  Also everytime a project is grabbed it gets the latest for the files

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,6 +1,8 @@
 const config = require('config');
 const projectService = require('../services/projectService');
 const historyService = require('../services/historyService');
+const BitesizeConfiguration = require('plucky-bitesize-pipeline').BitesizeConfiguration;
+const GitWrap = require('plucky-git').GitWrap;
 
 class ProjectController {
 	constructor(options) {
@@ -14,7 +16,13 @@ class ProjectController {
 			const promiseList = [];
 
 			projectList.forEach((projectConfig) => {
-				promiseList.push(historyService.getHistory(projectConfig.name).then((projectHistory) => {
+				const localConfig = projectConfig.localConfig;
+				promiseList.push(this.pullLatestBitesize(localConfig.bitesizeFolder, localConfig.bitesizeRepository).then(() => {
+					const biteSize = new BitesizeConfiguration(`${localConfig.bitesizeFolder}/environments.bitesize`);
+					const releaseObj = this.createReleaseObject(biteSize.getOrderedEnvironments(), localConfig.envAddons);
+					projectConfig.modules.release = releaseObj;
+					return historyService.getHistory(projectConfig.name);
+				}).then((projectHistory) => {
 					responseList.push({
 						projectConfig,
 						projectHistory
@@ -26,6 +34,24 @@ class ProjectController {
 				this.socket.emit('project:list', responseList);
 			});
 		});
+	}
+
+	createReleaseObject(orderedList, envAddons) {
+		const environmentObj = {};
+		orderedList.forEach((env) => {
+			let environment = env;
+			if(envAddons[env.name]) {
+				environment = Object.assign(env, envAddons[env.name]);
+			}
+			environmentObj[env.name] = environment;
+		});
+
+		return environmentObj;
+	}
+
+	pullLatestBitesize(directory, repository) {
+		const gitWrap = new GitWrap(directory, repository);
+		return gitWrap.clone();
 	}
 }
 
